99dd4a623b76d04af3663fc85ecbbd93a713a96b
Ajay Dharshan <ajaydharshan2005@gmail.com>
2025-05-23 10:36:03 +0530

    database schema done

--- backend/models/user.js
+++ backend/models/user.js
@@ -14,4 +14,4 @@ userSchema.pre('save', async function () {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema) ;
